fd83ede16ffb89d38067a1f396fa2d1b1cca3760
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2022-05-26 18:34:52 +0100

    feature: quit with button.

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import env
 import default
 import grid
 import graphical
+import pygame
 import time
 
 def main():
@@ -11,7 +12,12 @@ def main():
     agents = [agent.Random() for _ in range(num_agents)]
     with graphical.EnvironmentPrinter(map.grid) as printer:
         environment = env.Environment(map=map, init_taxis=num_agents, init_passengers=4, printer=printer)
-        for _ in range(100000000):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
             actions = [a.act() for a in agents]
             environment.step(*actions)
             environment.render()
